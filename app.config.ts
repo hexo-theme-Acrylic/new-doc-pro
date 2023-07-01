541981c9dd371b16bb32a904134b304c154ab924
timigomen <121149519+timigomen@users.noreply.github.com>
2023-07-01 13:26:56 +0800

    🤝 post add

---
+++ app.config.ts
@@ -0,0 +1,34 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Acrylic-Pro',
+    description: '创新绚彩Acrylic主题引领发展',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      github: 'hexo-theme-Acrylic/Hexo-Theme-Acrylic-Next'
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: false,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    },
+    footer: {
+      fluid: true,
+      iconLinks: [
+        {
+          href: 'https://jq.qq.com/?_wv=1027&k=qY1BMaBT',
+          icon: 'simple-icons:tencentqq'
+        }
+      ]
+    }
+  }
+})
